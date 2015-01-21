314b5c286dd07daab157cc4e512a36c22759b6df
sunjingyun <sunjingyun@worktile.com>
2015-01-21 14:59:38 +0800

    update

--- server/data/team.js
+++ server/data/team.js
@@ -7,7 +7,7 @@ var Team = function () {
     Team.prototype.addTeam = function (name, desc, domain, logo, callback) {
         return callback(null, {
             name  : name,
-            desc  : "",
+            desc  : desc,
             domain: domain,
             logo  : logo,
             id    : "11111"
@@ -17,7 +17,7 @@ var Team = function () {
     Team.prototype.updateTeam = function (id, name, desc, domain, logo, callback) {
         return callback(null, {
             name  : name,
-            desc  : "",
+            desc  : desc,
             domain: domain,
             logo  : logo,
             id    : id
